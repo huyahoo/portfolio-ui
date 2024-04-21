d5b5aa8c70517b722aba0d858f4e245db07e41e2
HUY <takasugi.tut@gmail.com>
2024-04-21 20:55:46 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolioui/',
   plugins: [
     vue(),
     VueI18nPlugin({
